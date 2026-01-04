26bbcd2ea918ef1cad69f0ba0caf15267b0c3320
europanite <europanite@gmail.com>
2026-01-04 22:11:07 +0900

    fix/npm

---
+++ src/banner-slot/index.ts
@@ -0,0 +1,18 @@
+export { BannerSlot } from "./banner-slot/BannerSlot";
+export { BannerSlotCard } from "./banner-slot/BannerSlotCard";
+
+
+export type {
+  BannerSlotBanner,
+  BannerSlotProps,
+  BannerSlotCardProps,
+  BannerSlotVariant,
+  BannerSlotTheme,
+
+  // Alias types
+  BannerSlotBanner as SlotBanner,
+  BannerSlotProps as SlotProps,
+  BannerSlotCardProps as SlotCardProps,
+  BannerSlotVariant as SlotVariant,
+  BannerSlotTheme as SlotTheme,
+} from "./banner-slot/types";
